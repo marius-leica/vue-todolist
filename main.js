90e18547dc31838fa87022f7356a623dda90c003
marius-leica <mariusleica@gmail.com>
2022-06-13 16:54:36 +0200

    Bonus 2

--- main.js
+++ main.js
@@ -20,6 +20,8 @@ Bonus:
 
 # 1- oltre al click sul pulsante, intercettare anche il tasto ENTER per aggiungere il todo alla lista
 
+# 2- cliccando sul testo dell’item, invertire il valore della proprietà done del todo corrispondente (se done era uguale a false, impostare true e viceversa)
+
 */
 
 const todos = [
